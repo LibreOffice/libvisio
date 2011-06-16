260b87195ec65080950611b05c4434272a6b3ff3
Tibby Lickle <tibbylickle@gmail.com>
2011-06-16 18:31:59 +0100

    Added basic ArcTo support

--- src/lib/VSD11Parser.cpp
+++ src/lib/VSD11Parser.cpp
@@ -471,6 +471,49 @@ void libvisio::VSD11Parser::shapeChunk(VSDInternalStream &stream, libwpg::WPGPai
       stream.seek(header.dataLength+header.trailer-18, WPX_SEEK_CUR);
     }
     break;
+    case 0x8c: // ArcTo
+    {
+      stream.seek(1, WPX_SEEK_CUR);
+      double x2 = readDouble(&stream) + xform.x;
+      stream.seek(1, WPX_SEEK_CUR);
+      double y2 = (xform.height - readDouble(&stream)) + xform.y;
+      stream.seek(1, WPX_SEEK_CUR);
+      double bow = readDouble(&stream);
+
+      rotatePoint(x2, y2, xform);
+      flipPoint(x2, y2, xform);
+
+      if (bow == 0)
+      {
+        x = x2; y = y2;
+        WPXPropertyList end;
+        end.insert("svg:x", x);
+        end.insert("svg:y", y);
+        end.insert("libwpg:path-action", "L");
+        m_currentGeometry[header.id] = end;
+      }
+      else
+      {
+        WPXPropertyList arc;
+        double chord = sqrt(pow((y2 - y),2) + pow((x2 - x),2));
+        double radius = (4 * bow * bow + chord * chord) / (8 * fabs(bow));
+        bool largeArc = fabs(bow) > radius ? 1 : 0;
+        bool sweep = bow < 0 ? 1 : 0;
+        x = x2; y = y2;
+        arc.insert("svg:rx", radius);
+        arc.insert("svg:ry", radius);
+        arc.insert("libwpg:rotate", xform.angle * (180/M_PI));
+        arc.insert("svg:large-arc", largeArc);
+        arc.insert("svg:sweep", sweep);
+        arc.insert("svg:x", x);
+        arc.insert("svg:y", y);
+        arc.insert("libwpg:path-action", "A");
+        m_currentGeometry[header.id] = arc;
+      }
+
+      stream.seek(header.dataLength+header.trailer-27, WPX_SEEK_CUR);
+    }
+      break;
     case 0x8f: // Ellipse
     {
       WPXPropertyList ellipse;
