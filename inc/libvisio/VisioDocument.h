5bd7231abf5e138d6b31faeb4cf2466e6a53f716
David Tardon <dtardon@redhat.com>
2014-08-29 13:31:09 +0200

    only export public symbols

--- inc/libvisio/VisioDocument.h
+++ inc/libvisio/VisioDocument.h
@@ -18,8 +18,12 @@
 #else
 #define VSDAPI __declspec(dllimport)
 #endif
+#else // !DLL_EXPORT
+#ifdef LIBVISIO_VISIBILITY
+#define VSDAPI __attribute__((visibility("default")))
 #else
 #define VSDAPI
+#endif
 #endif
 
 namespace libvisio
