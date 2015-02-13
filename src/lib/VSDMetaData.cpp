9686192d63bd208df1c6b53c892ba5a680cdc4b6
Richard PALO <richard@NetBSD.org>
2015-02-13 17:58:07 +0100

    use std::pow in order to avoid compile time errors like: error: call of overloaded 'pow(int, uint16_t&)' is ambiguous
    as seen on SunOS 5.11
    
    Change-Id: I2ac60caa2b0c3731064cdea6150523a00647036e
    Reviewed-on: https://gerrit.libreoffice.org/14481
    Tested-by: David Tardon <dtardon@redhat.com>
    Reviewed-by: David Tardon <dtardon@redhat.com>

--- src/lib/VSDMetaData.cpp
+++ src/lib/VSDMetaData.cpp
@@ -210,7 +210,7 @@ bool libvisio::VSDMetaData::parseTimes(librevenge::RVNGInputStream *input)
   uint32_t firstDirSectorLocation = readU32(input);
 
   // Seek to the Root Directory Entry
-  size_t sectorSize = pow(2, sectorShift);
+  size_t sectorSize = std::pow(2, sectorShift);
   input->seek((firstDirSectorLocation + 1) * sectorSize, librevenge::RVNG_SEEK_SET);
   // DirectoryEntryName: 64 bytes
   // DirectoryEntryNameLength: 2 bytes
