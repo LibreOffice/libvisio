LibreOffice/libvisio

commit c156c530df29972d6c9391348ec4351809cfd2e2

ofz#2307 avoid division by 0
Change-Id: I963ea7f7d7349c2614572b101be559b2f365a803

src/lib/VSDContentCollector.cpp
@@ -51,16 +51,25 @@ void computeRounding(double &prevX, double &prevY, double x0, double y0, double 
     angle -= M_PI;
     sweep = !sweep;
   }
-  double q = fabs(rounding / tan(angle / 2.0));
-  if (q > prevHalfLength)
+  double t = tan(angle / 2.0);
+  double q;
+  if (t != 0)
   {
-    q = prevHalfLength;
-    rounding = fabs(q*tan(angle / 2.0));
+    q = fabs(rounding / t);
+    if (q > prevHalfLength)
+    {
+      q = prevHalfLength;
+      rounding = fabs(q * t);
+    }
+    if (q > halfLength)
+    {
+      q = halfLength;
+      rounding = fabs(q * t);
+    }
   }
-  if (q > halfLength)
+  else
   {
-    q = halfLength;
-    rounding = fabs(q*tan(angle / 2.0));
+    q = fabs(rounding);
   }
   newX0 = x0-q*cos(lambda1);
   newY0 = y0-q*sin(lambda1);

commit d37e5ea5d633307a9b828ba67141520112999cad

fix rounding regression
... caused by commit c156c530df29972d6c9391348ec4351809cfd2e2 .

Change-Id: I83b50b5d0c31e5c58ea812ed007e6b40ea009883

src/lib/VSDContentCollector.cpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include <cassert>
 #include <string.h> // for memcpy
+#include <limits>
 #include <set>
 #include <stack>
 #include <boost/spirit/include/qi.hpp>
@@ -52,24 +53,18 @@ void computeRounding(double &prevX, double &prevY, double x0, double y0, double 
     sweep = !sweep;
   }
   double t = tan(angle / 2.0);
-  double q;
-  if (t != 0)
+  if (!(t < 0 || t > 0))
+    t = std::numeric_limits<double>::epsilon();
+  double q = fabs(rounding / t);
+  if (q > prevHalfLength)
   {
-    q = fabs(rounding / t);
-    if (q > prevHalfLength)
-    {
-      q = prevHalfLength;
-      rounding = fabs(q * t);
-    }
-    if (q > halfLength)
-    {
-      q = halfLength;
-      rounding = fabs(q * t);
-    }
+    q = prevHalfLength;
+    rounding = fabs(q * t);
   }
-  else
+  if (q > halfLength)
   {
-    q = fabs(rounding);
+    q = halfLength;
+    rounding = fabs(q * t);
   }
   newX0 = x0-q*cos(lambda1);
   newY0 = y0-q*sin(lambda1);